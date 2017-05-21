8375ab368bfd5c3434e940f8b1f288ef5a9110f9
barnabastoth <barnabastoth94@gmail.com>
2017-05-21 23:14:25 +0200

    menu.py updated, prettier, more functionality

--- menu.py
+++ menu.py
@@ -1,22 +1,28 @@
-def sql_menu(options, allowed_inputs):
-    print("Choose from one of the following options: ")
-    print("0: Exit")
-    print("1: Full name of all the mentors")
-    print("2: Nick names of the mentors at Miskolc")
-    print("3: Full name of Carol the applicant")
-    print("4: Full name of Applicant from Adipiscingenimmi")
-    print("5: Add Markus Schaffarzyk to the Applicants")
-    print("6: Update phone number of Jemina Foreman")
-    print("7: Delete Applicants with @mauriseu.net email adress ending")
+import os
+
+
+def sql_menu(options, allowed_inputs, menu_options):
+    for option in menu_options:
+        print(option)
     incorrect_input = True
     while incorrect_input:
         try:
             chosen_option = int(input("Enter a number between 1 and {0}: ".format(len(allowed_inputs))))
             if chosen_option in allowed_inputs:
                 if chosen_option == 7:
+                    os.system('clear')
+                    for option in menu_options:
+                        print(option)
+                    print("\n" + "Result of your query is: " + "\n")
                     print("Applicant with email @mauriseu.net has been removed from the database")
                     options[chosen_option]()
-                options[chosen_option]()
+
+                else:
+                    os.system('clear')
+                    for option in menu_options:
+                        print(option)
+                    print("\n" + "Result of your query is: " + "\n")
+                    options[chosen_option]()
             elif chosen_option == 0:
                 incorrect_input = False
             else:
